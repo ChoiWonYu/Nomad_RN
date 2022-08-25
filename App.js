237d39468f58779687005b51be4d5d55c23516cf
ChoiWonYu <phillip07290@naver.com>
2022-08-25 16:12:47 +0900

    feat: todoList

--- App.js
+++ App.js
@@ -6,87 +6,19 @@ import {
   Dimensions,
   ActivityIndicator,
 } from "react-native";
-import { useState, useEffect } from "react";
-import axios from "axios";
-const WINDOW_WIDTH = Dimensions.get("screen").width;
+import WeatherApp from "./WeatherAPP/WeaterApp";
+import TodoAPP from "./TodoAPP/TodoAPP";
 export default function App() {
-  const [city, setCity] = useState(null);
-  const [weather, setWeather] = useState(null);
-  const getInfo = async () => {
-    try {
-      await axios(
-        `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=ecbc2a08cf96f70a2c0c35f2f84282f1&units=metric`
-      ).then((res) => {
-        setCity(res.data.city.name);
-        const weatherData = res.data.list.filter((data, index) => index < 5);
-        setWeather(weatherData);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  useEffect(() => {
-    getInfo();
-  }, []);
   return (
-    <View style={style.container}>
-      <View style={style.city}>
-        {city ? (
-          <Text style={style.cityName}>{city}</Text>
-        ) : (
-          <ActivityIndicator size="large" />
-        )}
-      </View>
-      <ScrollView
-        horizontal
-        pagingEnabled
-        contentContainerStyle={style.weather}
-        showsHorizontalScrollIndicator={false}
-      >
-        {weather ? (
-          weather.map((list, index) => (
-            <View key={index} style={style.day}>
-              <Text style={style.temp}>{parseInt(list.main.temp)}</Text>
-              <Text style={style.weatherName}>{list.weather[0].main}</Text>
-            </View>
-          ))
-        ) : (
-          <View style={style.day}>
-            <ActivityIndicator size="large" />
-          </View>
-        )}
-      </ScrollView>
+    <View style={stlyes.container}>
+      <TodoAPP />
     </View>
   );
 }
 
-const style = StyleSheet.create({
-  city: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cityName: {
-    fontSize: 50,
-    color: "white",
-  },
-  day: {
-    width: WINDOW_WIDTH,
-    flex: 3,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  temp: {
-    fontSize: 150,
-    color: "white",
-  },
-  weatherName: {
-    fontSize: 40,
-    marginTop: -30,
-    color: "white",
-  },
+const stlyes = StyleSheet.create({
   container: {
+    backgroundColor: "black",
     flex: 1,
-    backgroundColor: "skyblue",
   },
 });
---
+++ TodoAPP/TodoAPP.js
@@ -0,0 +1,166 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { theme } from "./colors";
+import { useState, useEffect } from "react";
+import { AntDesign } from "@expo/vector-icons";
+const TODO_KEY = "@todo_key";
+const TodoAPP = () => {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
+  useEffect(() => {
+    loadTodos();
+    console.log(todos);
+  }, []);
+
+  const saveTodos = async (ToDos) => {
+    try {
+      await AsyncStorage.setItem(TODO_KEY, JSON.stringify(ToDos));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const removeTodos = async () => {
+    try {
+      await AsyncStorage.removeItem(TODO_KEY);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const loadTodos = async () => {
+    try {
+      const ToDos = await AsyncStorage.getItem(TODO_KEY);
+      return ToDos ? setTodos(JSON.parse(ToDos)) : {};
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const onDelete = (key) => {
+    Alert.alert("Delete To Do", "Are you sure?", [
+      {
+        text: "Cancel",
+      },
+      {
+        text: "I`m Sure",
+        style: "destructive",
+        onPress: () => {
+          const newTodo = { ...todos };
+          delete newTodo[key];
+          saveTodos(newTodo);
+          setTodos(newTodo);
+        },
+      },
+    ]);
+  };
+  const textDone = async () => {
+    if (text === "") return;
+    const newTodo = {
+      ...todos,
+      [Date.now()]: { text, working },
+    };
+
+    setTodos(newTodo);
+    await saveTodos(newTodo);
+    setText("");
+    console.log(todos);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.textBox}>
+        <TouchableOpacity onPress={() => setWorking(true)}>
+          <Text
+            style={{
+              ...styles.titleText,
+              color: working ? "white" : theme.grey,
+            }}
+          >
+            Todo
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setWorking(false)}>
+          <Text
+            style={{
+              ...styles.titleText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder={
+          working ? '"What do you have to do?"' : "Where do you want to go?"
+        }
+        returnKeyType="done"
+        value={text}
+        onChangeText={(text) => setText(text)}
+        onSubmitEditing={textDone}
+      />
+      {Object.keys(todos).map(
+        (key) =>
+          todos[key].working === working && (
+            <View key={key} style={styles.listBox}>
+              <Text style={styles.list}>{todos[key].text}</Text>
+              <TouchableOpacity onPress={() => onDelete(key)}>
+                <AntDesign name="delete" size={20} color={theme.bg} />
+              </TouchableOpacity>
+            </View>
+          )
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "black",
+    flex: 1,
+  },
+  textBox: {
+    flexDirection: "row",
+    marginTop: 80,
+    marginHorizontal: 30,
+    justifyContent: "space-between",
+  },
+  titleText: {
+    color: "white",
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  inputStyle: {
+    backgroundColor: "white",
+    marginHorizontal: 20,
+    borderRadius: 30,
+    height: 55,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+    fontSize: 18,
+  },
+  listBox: {
+    flexDirection: "row",
+    backgroundColor: theme.grey,
+    marginHorizontal: 20,
+    height: 50,
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 20,
+    paddingHorizontal: 20,
+    marginVertical: 5,
+  },
+  list: {
+    color: "white",
+    fontSize: 18,
+  },
+});
+
+export default TodoAPP;
